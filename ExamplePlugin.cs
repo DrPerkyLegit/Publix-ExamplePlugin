692cfb7667410e27a17ae800e1e3f240ab411aaa
DrPerkyLegit <DrPerkyFrLegit@gmail.com>
2025-10-19 17:35:38 -0400

    update to support config

--- ExamplePlugin.cs
+++ ExamplePlugin.cs
@@ -6,16 +6,32 @@ using Publix_ExamplePlugin.EventListeners;
 
 namespace Publix_ExamplePlugin
 {
-    [PluginInfo("dev.drperky.exampleplugin", "Example Plugin", "0.1", "1.0.0")]
+    [PluginInfo("dev.drperky.exampleplugin", "Example Plugin", "0.2", "1.0.2")]
     [PluginDescription("Example Plugin For PublixExtension")] //Description isnt used at the time of writing this, so its optional
     public class ExamplePlugin : BasePlugin
     {
+        public class ExampleConfig
+        {
+            public string ExampleString { get; set; } = "DefaultValue";
+            public bool ExampleBool { get; set; } = true;
+            public float ExampleFloat { get; set; } = float.MaxValue;
+        }
+
+        ExampleConfig loadedConfig;
+
         public override void OnEnable()
         {
-            this.GetLogger().Info("Plugin Loaded :)");
-
             Publix.getEventManager().Register(this, new ExampleEventListener(this));
+
+            loadedConfig = this.GetConfigManager().GetConfig<ExampleConfig>();
+
+            this.GetLogger().Info($"Config Value: {loadedConfig.ExampleString}");
+
+            this.GetLogger().Info("Plugin Loaded :)");
         }
-        public override void OnDisable() { }
+        public override void OnDisable()
+        {
+            this.GetConfigManager().SaveConfig(loadedConfig);
+        }
     }
 }
